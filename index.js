c5db55ac24cc626c83f59b608488a88cf3c1232b
ceboulba <antoine@archipicture.fr>
2020-03-18 23:55:15 +0100

    1 file modified

--- index.js
+++ index.js
@@ -7,59 +7,100 @@
 
 //  class Personnage
 class Personnage {
-    constructor(pseudo, classe, sante, attaque, niveau = 1) {
-        this.pseudo = pseudo
-        this.classe = classe
-        this.sante = sante
-        this.attaque = attaque
-        this.niveau = niveau
-    }
+  constructor(pseudo, classe, sante, attaque, niveau = 1) {
+    this.pseudo = pseudo;
+    this.classe = classe;
+    this.sante = sante;
+    this.attaque = attaque;
+    this.niveau = niveau;
+  }
 
-    evoluer() {
-        this.niveau++
-        console.log(`
+  evoluer() {
+    this.niveau++;
+    console.log(`
     ${this.pseudo} passe au niveau ${this.niveau} !
-    `)
-    }
+    `);
+  }
 
-    verifierSante() {
-        if (this.sante <= 0) {
-            this.sante = 0
-        }
-        console.log(`
+  verifierSante() {
+    if (this.sante <= 0) {
+      this.sante = 0;
+    }
+    console.log(`
     ${this.pseudo} a perdu !
-    `)
-    }
+    `);
+  }
 
-    get informations() {
-        console.log(`
-        ${this.pseudo} ${this.classe} a ${this.sante} points de vie et est au niveau ${this.niveau}."
-        `)
-    }
+  get informations() {
+    console.log(`
+        ${this.pseudo} ${this.classe} a ${
+      this.sante
+    } points de vie et est au niveau ${this.niveau}."
+        `);
+  }
 }
 
 //  class Magicien
 class Magicien extends Personnage {
-    constructor(pseudo) {
-        super()
-        this.pseudo = pseudo
-        this.classe = 'magicien'
-        this.sante = 170
-        this.attaque = 90
-    }
-    
-    attaquer(ennemi) {
-        ennemi.sante -= this.attaque
-        console.log(`
-        ${this.pseudo} attaque ${ennemi.pseudo} en lançant un sort ${this.attaque} dégâts
-        `
-        )
-        this.evoluer()
-        ennemi.verifierSante()
-    }
+  constructor(pseudo) {
+    super();
+    this.pseudo = pseudo;
+    this.classe = "magicien";
+    this.sante = 170;
+    this.attaque = 90;
+  }
+
+  attaquer(ennemi) {
+    ennemi.sante -= this.attaque;
+    console.log(`
+        ${this.pseudo} attaque ${ennemi.pseudo} en lançant un sort ${
+      this.attaque
+    } dégâts
+        `);
+    this.evoluer();
+    ennemi.verifierSante();
+  }
+
+  coupSpecial(ennemi) {
+    ennemi.sante -= this.attaque * 5;
+    console.log(`
+  ${this.pseudo} attaque avec son coup spécial puissance des arcanes ${
+      ennemi.pseudo
+    } ${this.attaque * 5} dégâts.
+  `);
+  }
 }
 
+//  class War
+class War extends Personnage {
+  constructor(pseudo) {
+    super();
+    this.pseudo = pseudo;
+    this.classe = "War";
+    this.sante = 350;
+    this.attaque = 50;
+  }
 
+  attaquer(ennemi) {
+    ennemi.sante -= this.attaque;
+    console.log(`
+        ${this.pseudo} attaque ${ennemi.pseudo} en lançant un sort ${
+      this.attaque
+    } dégâts
+        `);
+    this.evoluer();
+    ennemi.verifierSante();
+  }
 
-const M = new Magicien('Gandalf')
-M.informations
+  coupSpecial(ennemi) {
+    ennemi.sante -= this.attaque * 5;
+    console.log(`
+  ${this.pseudo} attaque avec son coup spécial puissance des arcanes ${
+      ennemi.pseudo
+    } ${this.attaque * 5} dégâts.
+  `);
+  }
+}
+
+const M = new Magicien("Gandalf");
+M.informations;
